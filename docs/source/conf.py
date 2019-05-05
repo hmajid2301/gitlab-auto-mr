bd3d6eaaa43d046abfaf70175e9cf295c14f4a1f
Haseeb Majid <hmajid2301@gmail.com>
2019-05-05 20:07:17 +0100

    :bug: Mock Imports
    Mock imports `gitlab` for docs generation (sphinx).

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,9 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# List of imports to mock
+autodoc_mock_imports = ["gitlab"]
+
 
 # -- Options for HTML output -------------------------------------------------
 
